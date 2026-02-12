0d70ec945e32f18fb461e6e875f3c4087f5975e5
risxt <risforshe@email.com>
2026-02-12 08:48:41 +0800

    v1.0 - NexaHub

---
+++ loader.lua
@@ -0,0 +1,5 @@
+-- NexaHub | TapSim Script
+-- Loader v1.0
+-- Usage: Copy this entire script and execute it
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/risxt/NexaHub/main/main.lua"))()
